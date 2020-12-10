af9c9681087112437197d11c82f550479910a348
Sm21 <sopmcg@yahoo.com>
2020-12-10 17:12:37 +0000

    Update end page

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ function results() {
   totalQuestions.classList.add("hidden");
   let str = score < 5 ? "Oops better luck next time!😤" : "Nice!😎";
   if (score == questions.length) str = "Excellent! You're a true fan!🎉";
-  let resultText = `<p>End of Quiz</p>
+  let resultText = `<p class="end">End of Quiz</p>
   <p><p>${str}<p/> You got ${score} out of ${questions.length} questions correct!</p>`;
   scores.innerHTML = resultText;
 }
